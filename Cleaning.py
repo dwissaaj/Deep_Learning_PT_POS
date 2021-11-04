01aae29cef9e82a2705a6bcb0ad9c566cc380fc9
dwisaji <dwis.buntelman123@gmail.com>
2021-11-04 08:58:05 +0700

    commit 4 nov

--- Cleaning.py
+++ Cleaning.py
@@ -1,11 +1,37 @@
+import pandas as pd
+from nltk.tokenize import TweetTokenizer
+df = pd.read_excel("Kompetitor Pos dan Pos indonesia.xlsx")
 import re
 
-def url(data):
-    url_pattern = re.compile(r'https?://\S+|www\.\S+')
-    data = url_pattern.sub(r'', data)
-    return data
 
-def email(data):
-    data = re.sub('\S*@\S*\s?', '', data)
-    return data
+df['text'] = df['text'].fillna('').apply(str)
+df['text'] = df['text'].replace({'"':'',
+                                 '\d+':'',
+                                 ':':'',
+                                 ';':'',
+                                 '#':'',
+                                 '_':'',
+                                 ',': '',
+                                 "'": '',
+                                  }, regex=True)
 
+df['text'] = df['text'].str.replace(r'[https]+[?://]+[^\s<>"]+|www\.[^\s<>"]+[?()]+[(??)]+[)*]+[(\xa0]+[-&gt...]', "",regex=True)
+
+df['text'] = df['text'].replace('\n','', regex=True)
+
+df['text'] = df['text'].replace({'\.':'','(/)':'','\(':'','\)':''},regex=True)
+df['text'] = df['text'].replace('[\.:"]','',regex =True)
+
+df['text'] = df['text'].fillna('').apply(str)
+
+
+
+df['text'].astype(str)
+letters_only = re.sub("[^a-zA-Z]",  # Search for all non-letters
+                          " ",          # Replace all non-letters with spaces
+                          str(df['text']))
+
+df['text'] = df['text'].str.strip()
+df['text'] = df['text'].str.lower()
+
+df.to_excel("kompetitor newest data 2.xlsx")
